ea060cfe5a58a6e1ae53969aabb86b00f737d240
arulhub <arul.murugan.1991@gmail.com>
2019-05-01 12:44:18 +0530

    1 file modified

--- src/app/directories/checkout.dir.ts
+++ src/app/directories/checkout.dir.ts
@@ -10,7 +10,7 @@ import { BrowserStorageServices } from './../services/storage.service';
     >
       <div class="col-sm-12">        
         <div class="alert alert-success" role="alert">
-          <h4 class="alert-heading">Dear FirstName LastName,</h4>
+          <h5 class="alert-heading">Dear FirstName LastName,</h5>
           <p>Thank you for your order. Your order has been placed successfully and will be delivered in <strong>3 working days</strong>.</p>
           <hr>
           <p class="mb-0">
@@ -20,13 +20,41 @@ import { BrowserStorageServices } from './../services/storage.service';
         </div>
       </div>
       <div class="col-sm-12">
-        
+        <div class="card">
+          <div class="card-header bg-info text-white">
+            <span><strong>Invoice Date: {{invoiceDate | date:"dd-MMM-yyyy"}}</strong></span>
+          </div>
+          <div class="card-body">
+            <div class="row">
+              <div class="col-sm-6 text-left">
+                Logo
+              </div>
+              <div class="col-sm-6 text-right">
+                Invoice #
+              </div>
+            </div>
+            <div class="row mt-1">
+              <div class="col-sm-6">                
+                  <div><span>From:</span></div>
+                  <div><strong>Shoppee Kart</strong></div>                
+                  <div><span>123, Anna Salai,</span></div>
+                  <div><span>Mount Road,</span></div>
+                  <div><span>Chennai - 600 001</span></div>
+                  <div><span>Mobile: 9876543210</span></div>
+              </div>
+              <div class="col-sm-6">
+                <p>To:</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>    
   `
 })
 
 export class CheckoutComponent{
+  public invoiceDate : any = new Date();
   public cart : any = {};
   public billingDetails : any = {};
   public cartFlag : boolean = false;
