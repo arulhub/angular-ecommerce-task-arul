94fc2794c499faee68de1b609d914d6ce429bbce
arulhub <arul.murugan.1991@gmail.com>
2019-05-07 21:29:28 +0530

    1 file modified

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   template : `
-  <div class="d-flex justify-content-center align-middle">
+  <div class="d-flex justify-content-center mt-5">
     <div class="card" style="width:50%">
       <div class="card-header">
         Login Page
@@ -16,6 +16,9 @@ import { Component } from '@angular/core';
           <div class="form-group row">
             <label for="pass" class="col-sm-2">Password</label>
             <input class="form-control col-sm-4" id="pass" type="password"/> 
+          </div>
+          <div class="form-group row">
+            <button class="btn btn-outline-info btn-sm col-sm-3">Login</button> 
           </div>
         </form>
       </div>
